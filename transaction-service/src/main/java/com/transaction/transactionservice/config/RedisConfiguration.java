19998db372ccdad5f6d72204bf1d4c2e6d16086b
vitorhugo-java <hugoalves.java@gmail.com>
2026-05-24 19:44:58 -0300

    feat: add Redis settings

--- transaction-service/src/main/java/com/transaction/transactionservice/config/RedisConfiguration.java
+++ transaction-service/src/main/java/com/transaction/transactionservice/config/RedisConfiguration.java
@@ -1,27 +1,28 @@
 package com.transaction.transactionservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.transaction.transactionservice.transaction.application.dto.TransactionResponse;
+import com.transaction.transactionservice.dto.response.TransactionResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
 
     @Bean
-    RedisTemplate<String, TransactionResponse> transactionResponseRedisTemplate(
-            RedisConnectionFactory connectionFactory,
-            ObjectMapper objectMapper
-    ) {
+    RedisTemplate<String, TransactionResponse> transactionResponseRedisTemplate(RedisConnectionFactory connectionFactory) {
+        var serializer = new JacksonJsonRedisSerializer<>(TransactionResponse.class);
+
         RedisTemplate<String, TransactionResponse> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
         template.afterPropertiesSet();
+
         return template;
     }
 }
