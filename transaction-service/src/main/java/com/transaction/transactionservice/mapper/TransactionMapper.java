a5734577b07a140958adf916505b150423799efc
vitorhugo-java <hugoalves.java@gmail.com>
2026-05-24 19:49:40 -0300

    feat: add mapper

---
+++ transaction-service/src/main/java/com/transaction/transactionservice/mapper/TransactionMapper.java
@@ -0,0 +1,42 @@
+package com.transaction.transactionservice.mapper;
+
+import com.transaction.transactionservice.dto.request.CreateTransactionRequest;
+import com.transaction.transactionservice.dto.response.TransactionResponse;
+import com.transaction.transactionservice.entity.Transaction;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(
+        componentModel = "spring"
+)
+public interface TransactionMapper {
+
+    @Mapping(
+            target = "id",
+            ignore = true
+    )
+    @Mapping(
+            target = "userId",
+            ignore = true
+    )
+    @Mapping(
+            target = "status",
+            ignore = true
+    )
+    @Mapping(
+            target = "createdAt",
+            ignore = true
+    )
+    @Mapping(
+            target = "updatedAt",
+            ignore = true
+    )
+    Transaction toEntity(
+            CreateTransactionRequest request
+    );
+
+    TransactionResponse toResponse(
+            Transaction transaction
+    );
+
+}
