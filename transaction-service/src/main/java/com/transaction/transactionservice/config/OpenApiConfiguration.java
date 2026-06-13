9052134105ec4d0d9439c2fd4831d49bf7ea693a
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-13 17:13:08 -0300

    feat: add gateway url on transaction service

--- transaction-service/src/main/java/com/transaction/transactionservice/config/OpenApiConfiguration.java
+++ transaction-service/src/main/java/com/transaction/transactionservice/config/OpenApiConfiguration.java
@@ -2,15 +2,21 @@ package com.transaction.transactionservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
 
+    @Value("${application.gateway-url:http://localhost:8080}")
+    private String gatewayUrl;
+
     @Bean
     OpenAPI transactionOpenApi() {
         return new OpenAPI()
+                .addServersItem(new Server().url(gatewayUrl + "/api/transactions").description("Gateway"))
                 .info(new Info()
                         .title("Finance Tracker Transaction Service")
                         .version("1.0.0")
